bfc86c436332473c37fe1a6204fe9da2c48fbe3d
MohamedMamdouh-98 <miido96@gmail.com>
2022-04-20 14:00:16 +0200

    Added a button to the scroll

--- js/mo.js
+++ js/mo.js
@@ -1,5 +1,17 @@
-const menu = document.getElementById('action')
-const icon = document.getElementById('menu')
+const menu = document.getElementById('action');
+const icon = document.getElementById('menu');
+const up = document.getElementById('up');
 icon.onclick = function() {
     menu.classList.toggle('menu-media');
+}
+
+window.onscroll = function() {
+    this.scrollY >= 590 ? up.classList.add('show'): up.classList.remove('show')
+}
+
+up.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    }) 
 }
